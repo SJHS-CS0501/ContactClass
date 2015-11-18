e4bec61c35cd7b9cf9179ab8e77d14234f7fff75
rysmith18 <rtsmith1101@outlook.com>
2015-11-18 08:34:43 -0500

    Added more stuff

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author woytek
+ * @author woytek/Ryan Smith
  */
 public class ContactTest {
 
@@ -11,19 +11,84 @@ public class ContactTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Contact testContact = new Contact();
-        String input = null;
-        Scanner keyboard = new Scanner(System.in);
+        Contact testContact = new Contact(); //creating reference
+        String input = null; //initializing String input
+        Scanner keyboard = new Scanner(System.in); //allows user to input information
         System.out.print( "Enter the last name: " );
         input = keyboard.nextLine();
         testContact.setLastName( input );
         
         System.out.println( "Last name: " + testContact.getLastName() );
+        
+        System.out.println("Enter the first name: ");
+        input = keyboard.nextLine();
+        testContact.setFirstName(input);
+        
+        System.out.println("First name: " + testContact.getFirstName() );
+        
+        System.out.println("Enter the middle name: ");
+        input = keyboard.nextLine();
+        testContact.setMiddleName(input);
+        
+        System.out.println("Middle name: " + testContact.getMiddleName() );
+        
+        System.out.println("Enter the phone number: ");
+        input = keyboard.nextLine();
+        testContact.setPhone(input);
+        
+        System.out.println("Phone number: " + testContact.getPhone() );
+        
+        System.out.println("Enter the prefix: ");
+        input = keyboard.nextLine();
+        testContact.setPrefix(input);
+        
+        System.out.println("Prefix: " + testContact.getPrefix() );
+        
+        System.out.println("Enter the email: ");
+        input = keyboard.nextLine();
+        testContact.setEmail(input);
+        
+        System.out.println("Email: " + testContact.getEmail() );
+        
+        System.out.println("Enter the street: ");
+        input = keyboard.nextLine();
+        testContact.setStreet(input);
+        
+        System.out.println("Street: " + testContact.getStreet() );
+        
+        System.out.println("Enter the city: ");
+        input = keyboard.nextLine();
+        testContact.setCity(input);
+        
+        System.out.println("City: " + testContact.getCity() );
+        
+        System.out.println("Enter the state: ");
+        input = keyboard.nextLine();
+        testContact.setState(input);
+        
+        System.out.println("State: " + testContact.getState() );
+        
+        System.out.println("Enter the occupation: ");
+        input = keyboard.nextLine();
+        testContact.setOccupation(input);
+        
+        System.out.println("Occupation: " + testContact.getOccupation() );
     }
+    
+    /**
+     * This class is made up of mutators and accessors.
+     * @author Ryan Smith
+     *
+     */
     
     public static class Contact {
         Contact() {
         }
+        
+        /**
+         * This method validifies the last name.
+         * @param n
+         */
         
         public void setLastName( String n ) {
             
@@ -33,20 +98,216 @@ public class ContactTest {
                 lName = n;
             }
         }
+        
+        /**
+         * This method accesses the last name.
+         * @return lname
+         */
         public String getLastName() {
             return lName;
         }
+        
+        /**
+         * This method validifies the first name.
+         * @param n
+         */
+        
         public void setFirstName( String n ) {
-            fName = n;
+        	if( !n.matches("^[A-Za-z ' -]+$") ) { //regular expression
+                System.out.println( "Invalid characters in first name" );
+            } else {
+            	fName = n;
+            }
+        	
         }
+        
+        /**
+         * This method accesses the first name
+         * @return fname
+         */
+        
         public String getFirstName() {
             return fName;
         }
+        
+        /**
+         * This method validifies the middle name.
+         * @param n
+         */
+        
         public void setMiddleName( String n ) {
-            mName = n;
+        	if( !n.matches("^[A-Za-z ' -]+$") ) { //regular expression
+                System.out.println( "Invalid characters in middle name" );
+            } else {
+            	mName = n;
+            }
+        	
         }
+        
+        /**
+         * This method accesses the middle name.
+         * @return 
+         */
+        
         public String getMiddleName() {
             return mName;
+        }
+        
+        /**
+         * This method validifies the phone number.
+         * @param n
+         */
+        
+        public void setPhone( String n ) {
+			if(!n.matches("^[0-9]{3}-[0-9]{3}-[0-9]{4}$")) { //regular expression
+                System.out.println("Invalid characters in phone number");
+            } else {
+            	phone = n;
+            }
+            
+        }
+        
+        /**
+         * This method accesses the phone number.
+         * @return phone
+         */
+        
+        public String getPhone() {
+            return phone;
+        }
+        
+        /**
+         * This method validifies the prefix.
+         * @param n
+         */
+        
+        public void setPrefix(String n) {
+			if(!n.matches("^[A-Za-z ' . -]+$") ) { //regular expression
+                System.out.println("Invalid characters in prefix");
+            } else {
+            	prefix = n;
+            }
+            
+        }
+        
+        /**
+         * This method accesses the prefix.
+         * @return prefix
+         */
+        
+        public String getPrefix() {
+        	return prefix;
+        }
+        
+        /**
+         * This method validifies the email.
+         * @param n
+         */
+        
+        public void setEmail(String n) { //regular expression
+			if(!n.matches("^[A-Za-z ' @ . -]+$")) {
+        		System.out.println("Invalid characters in email");
+        	} else {
+        		email = n;
+        	}
+        }
+        
+        /**
+         * This method accesses the  email
+         * @return email
+         */
+        
+        public String getEmail() {
+        	return email;
+        }
+        
+        /**
+         * This method validifies the street.
+         * @param n
+         */
+        
+        public void setStreet(String n) {
+        	if(!n.matches("^[A-Za-z ' . -]+$")) { //regular expression
+        		System.out.println("Invalid characters in street name");
+        	} else {
+        		street = n;
+        	}
+        	
+        }
+        
+        /**
+         * This method accesses the street
+         * @return street
+         */
+        
+        public String getStreet() {
+        	return street;
+        }
+        
+        /**
+         * This method validifies the city.
+         * @param n
+         */
+        
+        public void setCity(String n) {
+        	if(!n.matches("^[A-Za-z ' . -]+$")) { //regular expression
+        		System.out.println("Invalid characters in city name");
+        	} else {
+        		city = n;
+        	}
+        }
+        
+        /**
+         * This method accesses the city
+         * @return city
+         */
+        
+        public String getCity() {
+        	return city;
+        }
+        
+        /**
+         * This method validifies the state.
+         * @param n
+         */
+        
+        public void setState(String n) {
+        	if(!n.matches("^[A-Za-z ' . -]+$")) { //regular expression
+        		System.out.println("Invalid characters in state name");
+        	} else {
+        		state = n;
+        	}
+        }
+        
+        /**
+         * This method accesses the state
+         * @return state
+         */
+        
+        public String getState() {
+        	return state;
+        }
+        
+        /**
+         * This method validifies the occupation.
+         * @param n
+         */
+        
+        public void setOccupation(String n) {
+        	if(!n.matches("^[A-Za-z ' . -]+$")) { //regular expression
+        		System.out.println("Invalid characters in occupation");
+        	} else {
+        		occupation = n;
+        	}
+        }
+        
+        /**
+         * This method accesses the occupation.
+         * @return occupation
+         */
+        
+        public String getOccupation() {
+        	return occupation;
         }
         /*
          * Name (first, last, middle)
